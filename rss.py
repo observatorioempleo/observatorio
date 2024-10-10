3e8cd35dee8c723779812412135d758e2c0a643b
observatorioempleo <arodriguez.senes@gmail.com>
2024-10-10 09:44:22 +0200

    Update rss.py

--- rss.py
+++ rss.py
@@ -77,5 +77,5 @@ def index():
     return render_template('index.html', noticias=noticias, fuentes=fuentes, ambitos=ambitos, agentes=agentes, materias=materias)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
 
